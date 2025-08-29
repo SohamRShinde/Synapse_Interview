cfa1a967397bb7daede8f9c7ea50351ad1ec34ba
SohamRShinde <shinde.soham2117@gmail.com>
2025-08-29 16:52:09 +0530

    completed task-1.1

--- task-1/pokemon.py
+++ task-1/pokemon.py
@@ -1,5 +1,7 @@
+from itertools import combinations
+
 Pokedex = {
-"Pikachu": ("Electric"),
+"Pikachu": ("Electric",),
 "Charizard": ("Fire", "Flying"),
 "Lapras": ("Water", "Ice"),
 "Machamp": ("Fighting",),
@@ -10,3 +12,22 @@ Pokedex = {
 "Gengar": ("Ghost", "Poison"),
 "Onix": ("Rock", "Ground")
 }
+
+def allPossibleTeams(k) :
+    teams = combinations(Pokedex, k)
+    for t in teams:
+        print(t)
+
+def strongestTeam(k) :
+    maxNoOfTypes = 0
+    bestTeam = None
+    teams = combinations(Pokedex, k)
+    for team in teams:
+        noOfTypes = len(set().union(*(Pokedex[p] for p in team)))
+        if noOfTypes > maxNoOfTypes :
+            maxNoOfTypes = noOfTypes
+            bestTeam = team 
+    return bestTeam
+    
+
+print(strongestTeam(3))
